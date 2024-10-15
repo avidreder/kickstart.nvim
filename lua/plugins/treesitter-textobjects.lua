49a84ce6625a0477132f4d3f34779e257e46a7a8
Andrew Reder <avidreder@gmail.com>
2024-10-15 06:00:59 -0700

    break into modules, add textobjects, add lazygit

---
+++ lua/plugins/treesitter-textobjects.lua
@@ -0,0 +1,31 @@
+return {
+  'nvim-treesitter/nvim-treesitter-textobjects',
+  lazy = true,
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+            -- nvim_buf_set_keymap) which plugins like which-key display
+            ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+            -- You can also use captures from other query groups like `locals.scm`
+            ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
+          },
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V', -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = true,
+        },
+      },
+    }
+  end,
+}
