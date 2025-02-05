d396fe9af434417b1f3b282cb77270d83b4ad989
Andrew Reder <avidreder@gmail.com>
2025-02-05 13:10:30 -0500

    update setup script to add brew and terminal

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> /Users/andrewreder/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/andrewreder/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install --cask ghostty
 brew install neovim
 brew install ripgrep
 brew install tmux 
